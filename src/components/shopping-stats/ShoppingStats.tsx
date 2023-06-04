f4bbeedabca41b2fd5704289d6b6437ccdb40d4a
zillurclub <zillurdeu@gmail.com>
2023-06-04 21:25:15 +0200

    feat: add monthly chart

--- src/components/shopping-stats/ShoppingStats.tsx
+++ src/components/shopping-stats/ShoppingStats.tsx
@@ -1,6 +1,15 @@
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import foodHistory from "../../utils/constant/foodHistory";
 import { getTopItemAndCategory } from "../../utils/helpers/getTopItem";
+import { formatMonthlyData } from "../../utils/helpers/formatMonthylData";
 
 const ShoppingStats = () => {
   const { items, categories, topItems, topCategories, totalItem } =
@@ -26,6 +35,8 @@ const ShoppingStats = () => {
       customClass: "top-cat",
     },
   ];
+
+  const formattedGrocery = formatMonthlyData(foodHistory);
   return (
     <div className="main__content-stats">
       <div className="container">
@@ -37,12 +48,18 @@ const ShoppingStats = () => {
                 <h2>{title}</h2>
 
                 <ul>
-                  {top.map((item) => (
-                    <li key={item} className="d-flex justify-content-between">
-                      <span>{item}</span>
-                      <span>{getPercent(items[item], totalItem)}</span>
-                    </li>
-                  ))}
+                  {top.map((item) => {
+                    let percent = getPercent(items[item], totalItem);
+                    return (
+                      <li key={item} className="d-flex justify-content-between">
+                        <span>{item}</span>
+                        <span>{percent}%</span>
+                        <span
+                          className={`w-100 d-block progress-bar progress-${percent}`}
+                        ></span>
+                      </li>
+                    );
+                  })}
                 </ul>
               </article>
             );
@@ -54,12 +71,24 @@ const ShoppingStats = () => {
         <h2>Monthly Summary</h2>
 
         <div>
-          <LineChart width={600} height={400}>
-            <Line type={"monotone"} dataKey={"uv"} stroke="#8884d8" />
+          <LineChart
+            width={600}
+            height={400}
+            data={formattedGrocery}
+            margin={{
+              top: 10,
+              right: 10,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            <Line type={"monotone"} dataKey={"items"} stroke="#F9A109" />
 
-            <CartesianGrid stroke="#ccc" />
+            <CartesianGrid stroke="#E0E0E0" />
             <XAxis dataKey={"name"} />
             <YAxis />
+            <Tooltip />
+            <Legend />
           </LineChart>
         </div>
       </section>
---
+++ src/utils/helpers/formatMonthylData.ts
@@ -0,0 +1,34 @@
+import { format } from "date-fns";
+import groceryByMonth from "./groceryByMonth";
+
+export const formatMonthlyData = (foodHistory: any) => {
+  // get foodHistory or shopping history and format data by month
+  const { formattedGrocery: grocery } = groceryByMonth(foodHistory);
+  const formattedMonth: { [month: string]: number } = {};
+
+  for (let i = 0; i < 12; i++) {
+    formattedMonth[format(new Date(2023, i, 1), "LLLL")] = 0;
+  }
+
+  for (let time of Object.keys(grocery)) {
+    let month = time.split(" ")[0];
+    let monthItems = 0;
+
+    for (let g of grocery[time]) {
+      for (let item of g.items) {
+        monthItems += item.pieces as number;
+      }
+    }
+
+    if (formattedMonth.hasOwnProperty(month)) {
+      formattedMonth[month] += monthItems;
+    } else {
+      formattedMonth[month] = monthItems;
+    }
+  }
+
+  return Object.keys(formattedMonth).map((item) => ({
+    name: item,
+    items: formattedMonth[item],
+  }));
+};
