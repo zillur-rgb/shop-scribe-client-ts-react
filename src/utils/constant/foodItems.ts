e3c04afe1deaf37f0f16d4fba917332926ef47e1
zillurclub <zillurdeu@gmail.com>
2023-06-04 14:31:02 +0200

    chore: update food items list

--- src/utils/constant/foodItems.ts
+++ src/utils/constant/foodItems.ts
@@ -1,6 +1,6 @@
 import { IFoodItem } from "../../types/types";
 
-export const foodItems: IFoodItem[] = [
+const foodItems: IFoodItem[] = [
   {
     id: 1,
     name: "Avocado",
@@ -75,53 +75,147 @@ export const foodItems: IFoodItem[] = [
   },
   {
     id: 9,
-    name: "",
-    category: "Fruit and vegetables",
-    image: "",
-    description: "",
+    name: "Chicken leg box",
+    category: "Meat and Fish",
+    image:
+      "https://media.istockphoto.com/id/1364102953/photo/friends-eating-chicken-wings.jpg?s=1024x1024&w=is&k=20&c=Fd6W0fQMOsTo0SUocapNfBsUdvOaCJjHTLXr82ET4tw=",
+    description:
+      "Chicken legs are stuffed -just under the skin -with a buttery dressing that features fresh garlic, parsley, bread crumbs and other tasty ingredients. Then the legs are slathered with a curry and garlic butter and grilled to perfection. Instant mashed potato flakes make a yummy coating for baked chicken.",
   },
   {
     id: 10,
-    name: "",
-    category: "Fruit and vegetables",
-    image: "",
-    description: "",
+    name: "Chicken 1kg",
+    category: "Meat and Fish",
+    image:
+      "https://images.unsplash.com/photo-1501200291289-c5a76c232e5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    description:
+      "For a family-friendly dish that's comforting and hearty, chicken is your best bet! Check out our best chicken.",
   },
   {
     id: 11,
-    name: "",
-    category: "Fruit and vegetables",
-    image: "",
-    description: "",
+    name: "Pork fillets 450g",
+    category: "Meat and Fish",
+    image:
+      "https://images.unsplash.com/photo-1671193238908-d53616285ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1882&q=80",
+    description:
+      "Pork fillet recipes Also known as the tenderloin, this is the eye fillet that comes from within the loin. It's a lazy muscle and as such is lean and very tender",
   },
   {
     id: 12,
-    name: "",
-    category: "Fruit and vegetables",
-    image: "",
-    description: "",
+    name: "Salmon 1kg",
+    category: "Meat and Fish",
+    image:
+      "https://images.unsplash.com/photo-1509402308-817902776267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1909&q=80",
+    description:
+      "Salmon is one of the most nutritious foods on the planet. This popular fatty fish is not only loaded with nutrients but also may reduce certain risk factors for several diseases.",
   },
   {
     id: 13,
-    name: "",
-    category: "",
-    image: "",
-    description: "",
+    name: "Avocado",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/510015094/photo/avocado-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=t1Ot8g5gMeNIYhtA-BYbS8DYoveBYfOm-FTgr1GD90c=",
+    description:
+      "Nutrient-dense foods are those that provide substantial amounts of vitamins, minerals and other nutrients with relatively few calories. One-third of a medium avocado (50 g) has 80 calories and contributes nearly 20 vitamins and minerals, making it a great nutrient-dense food choice.",
   },
   {
     id: 14,
-    name: "",
-    category: "",
-    image: "",
-    description: "",
+    name: "Banana",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/173242750/photo/banana-bunch.jpg?s=1024x1024&w=is&k=20&c=mzktjTrLz_ZdKClKR5btvo5cBY-BJ4h4QOT8LVflB2M=",
+    description:
+      "Bananas contain fiber as well as vitamins and minerals that may provide health benefits. Bananas are among the most important food crops on the planet. Bananas are a healthy source of fiber, potassium, vitamin B6, vitamin C, and various antioxidants and phytonutrients.",
   },
-
   {
     id: 15,
-    name: "",
-    category: "",
-    image: "",
-    description: "",
+    name: "Bunch of carrots 5pcs",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/1388403435/photo/fresh-carrots-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=AZlR713OB95BftaWVrKHOLKcbX1VYSWBHfGs1C6_nj8=",
+    description:
+      "Carrots are rich in vitamins, minerals, and antioxidant compounds. As part of a balanced diet, they can help support immune function, reduce the risk of some cancers and promote wound healing and digestive health.",
+  },
+  {
+    id: 16,
+    name: "Potato",
+    category: "Beverages",
+    image:
+      "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    description:
+      "The potato is a starchy food, a tuber of the plant Solanum tuberosum and is a root vegetable native to the Americas. The plant is a perennial in the nightshade family Solanaceae. Wild potato species can be found from the southern United States to southern Chile. ",
+  },
+  {
+    id: 17,
+    name: "Pre-cooked corn 450g",
+    category: "Beverages",
+    image:
+      "https://images.unsplash.com/photo-1551754655-cd27e38d2076?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    description:
+      "Corn is considered both a vegetable and a cereal grain.Sweet corn that you eat off the cob is usually considered a vegetable in the culinary world, whereas the dry seeds that are used for popcorn are classified as whole grains.",
+  },
+  {
+    id: 18,
+    name: "Mandarin Nadorcott",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/1331360969/photo/healthy-fruit-with-vitamin-c-mandarina-afourer-y-nadorcott-tango-valley-gold-orri-moria.jpg?s=1024x1024&w=is&k=20&c=27XVwWESRCLix10yoaoymJITGX8b8KE8pm2gk-JuSW8=",
+    description:
+      "Nadorcott is a high quality, mid to late-maturing Clementine-type of mandarin. It is a fruit with great depth of flavour and sweetness and a good acidity balance. The smooth, deep orange peel is fairly tightly adhering to the segments, but is easily removed. Nadorcott on sale in UK is practically seedless, but seeds can occassionally be found.",
+  },
+  {
+    id: 19,
+    name: "Piele De Sapo Melon",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/624283996/photo/group-of-green-melons-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=31Ll3rccFQ5WjjJh3FqHzCc1OUTBvwrCCBk8uc3b0eo=",
+    description:
+      "The Santa Claus melon, sometimes known as Christmas melon or Piel de Sapo. The flesh of uncut melons is juicier and softer if kept at room temperature one or two days before serving. Once ripened or cut, it should be refrigerated in plastic.",
+  },
+  {
+    id: 20,
+    name: "Watermelon",
+    category: "Beverages",
+    image:
+      "https://media.istockphoto.com/id/1321448019/photo/ripe-juicy-watermelon-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=hTEGWeUIiXRtCUwpfZti4D6415228VAKEuF4w8ZEWeo=",
+    description:
+      "Watermelon (Citrullus lanatus) is a flowering plant species of the Cucurbitaceae family and the name of its edible fruit. A scrambling and trailing vine-like plant, it is a highly cultivated fruit worldwide, with more than 1,000 varieties.",
+  },
+  {
+    id: 21,
+    name: "Chicken leg box",
+    category: "Pets",
+    image:
+      "https://media.istockphoto.com/id/1364102953/photo/friends-eating-chicken-wings.jpg?s=1024x1024&w=is&k=20&c=Fd6W0fQMOsTo0SUocapNfBsUdvOaCJjHTLXr82ET4tw=",
+    description:
+      "Chicken legs are stuffed -just under the skin -with a buttery dressing that features fresh garlic, parsley, bread crumbs and other tasty ingredients. Then the legs are slathered with a curry and garlic butter and grilled to perfection. Instant mashed potato flakes make a yummy coating for baked chicken.",
+  },
+  {
+    id: 22,
+    name: "Chicken 1kg",
+    category: "Pets",
+    image:
+      "https://images.unsplash.com/photo-1501200291289-c5a76c232e5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    description:
+      "For a family-friendly dish that's comforting and hearty, chicken is your best bet! Check out our best chicken.",
+  },
+  {
+    id: 23,
+    name: "Pork fillets 450g",
+    category: "Pets",
+    image:
+      "https://images.unsplash.com/photo-1671193238908-d53616285ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1882&q=80",
+    description:
+      "Pork fillet recipes Also known as the tenderloin, this is the eye fillet that comes from within the loin. It's a lazy muscle and as such is lean and very tender",
+  },
+  {
+    id: 24,
+    name: "Salmon 1kg",
+    category: "Pets",
+    image:
+      "https://images.unsplash.com/photo-1509402308-817902776267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1909&q=80",
+    description:
+      "Salmon is one of the most nutritious foods on the planet. This popular fatty fish is not only loaded with nutrients but also may reduce certain risk factors for several diseases.",
   },
 ];
 
